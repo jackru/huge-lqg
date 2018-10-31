73ca40d685d230964b3f4fcfaedec08c1133218f
jackru <jack132@gmail.com>
2018-10-31 13:28:09 +0000

    Actually makes interaction generator more memory efficient
    Last commit said it did this, but actually just changed np.random.rand to randn for
    the base variable generation.

--- toy-modelling/toy_data_generation.py
+++ toy-modelling/toy_data_generation.py
@@ -47,36 +47,39 @@ def generate_poisson_data(nrows, nvars, binary_fraction=1.0, binary_imbalance=3,
     return (X, y)
 
 
-def generate_interaction_data(nrows, nvars,
+INTERACTION_FUNCS = [
+    lambda x, y: abs(x - y),
+    lambda x, y: x * y,
+    lambda x, y: abs(x * y),
+]
+
+
+def generate_interaction_data(nrows, nvars, interaction_funcs=INTERACTION_FUNCS,
                               noise_func=lambda x: (x ** 0.5) * 0.1,
                               const_func=lambda x: np.random.randn() * (x ** 0.5),
                               random_state=None):
+    '''
+    Here's the docstring!
+    '''
     np.random.seed(random_state)
     data = pd.DataFrame(index=range(nrows))
     for varnum in range(nvars):
         data[f'x{varnum}'] = np.random.randn(nrows)
-
-    for i in range(nvars - 1):
-        for j in range(i+1, nvars):
-            func = np.random.choice([
-                lambda x, y: abs(x - y),
-                lambda x, y: x * y,
-                lambda x, y: abs(x * y),
-            ])
-            data[f'i{i}j{j}'] = func(data[f'x{i}'], data[f'x{j}'])
-
     data['gaussian_noise'] = np.random.randn(nrows) * noise_func(nvars)
     const = const_func(nvars)
     xvars = [x for x in data if x.startswith('x')]
-    interactions = [ij for ij in data if ij.startswith('i')]
     coefs = pd.Series(np.random.randn(nvars))
-    coefs_interaction = pd.Series(np.random.randn(int(nvars * (nvars - 1) / 2)))
-    data['y_interactions'] = (
-        data[xvars].dot(coefs.values)
-        + data[interactions].dot(coefs_interaction.values)
-        + data['gaussian_noise']
-        + const
-    )
+    data['y_interactions'] = data[xvars].dot(coefs.values) + data['gaussian_noise'] + const
+
+    for i in range(nvars - 1):
+        for j in range(i+1, nvars):
+            func = np.random.choice(interaction_funcs)
+            coef = np.random.randn()
+            data['y_interactions'] += (
+                coef * func(data[f'x{i}'], data[f'x{j}'])
+            )
+
     X = data[xvars]
     y = data['y_interactions']
+
     return (X, y)
