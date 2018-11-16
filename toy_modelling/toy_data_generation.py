2e1cd3265025159390d4041121724c6649437477
jackru <jack132@gmail.com>
2018-11-16 10:13:09 +0000

    Poisson and gamma generators now separated nicely

--- toy_modelling/toy_data_generation.py
+++ toy_modelling/toy_data_generation.py
@@ -16,7 +16,7 @@ def generate_binvar(nrows, exponent_of_imbalance=3, random_state=None):
     :param int nrows: the number of binary observations returned
     :param positive, numeric exponent_of_imbalance: determines likelhood of
         variable skewing positive (exponent below 1) or negative (over 1)
-    :param int random_state: specify for reproducible results
+    :param int random_state: specify for reproducable results
     :return array of binary-valued floats:
     """
     np.random.seed(random_state)
@@ -38,7 +38,7 @@ def generate_x_data(nrows, nvars, binary_fraction=1.0, binary_imbalance=3,
         variables skewing positive (exponent below 1) or negative (over 1)
     :param float continuous_scaling_factor: scale of continuous variable value
         range relative to binary variables
-    :param int random_state: specify for reproducible results
+    :param int random_state: specify for reproducable results
     :return pd.DataFrame: the data
     """
     np.random.seed(random_state)
@@ -72,7 +72,7 @@ def generate_systematic_y(x_data, terms=[(1, 1.0)],
     :param list[functions] interaction_funcs: a list of functions specifying
         variable interactions
     :param bool debug: if True, returns information on the generative model
-    :param int random_state: specify for reproducible results
+    :param int random_state: specify for reproducable results
     :return pd.Series: the response variable
     :return dict: (optional) the debug info
     """
@@ -118,7 +118,7 @@ def generate_linear_data(nrows, nvars, binary_fraction=1.0, binary_imbalance=3,
     :param int nrows: the number of data rows output
     :param int nvars: the number of predictor variables
     :param f(nvars) noise_func: determines scale of noise as function of nvars
-    :param int random_state: specify for reproducible results
+    :param int random_state: specify for reproducable results
     :return tuple(pd.DataFrame, pd.Series): predictors and target variable
     """
     np.random.seed(random_state)
@@ -138,7 +138,8 @@ def generate_linear_data(nrows, nvars, binary_fraction=1.0, binary_imbalance=3,
 
 def generate_poisson_data(nrows, nvars, binary_fraction=1.0,
                           binary_imbalance=3, continuous_scaling_factor=0.5,
-                          coefs_scaling_factor=0.2, const=np.log(0.01),
+                          terms=[(1, 1.0)], interaction_funcs=ARRAY_LIST_FUNCS,
+                          scale_to_range=(np.log(0.0003), np.log(0.3)),
                           random_state=None):
     """
     Generates data using a Poisson generative model
@@ -150,17 +151,20 @@ def generate_poisson_data(nrows, nvars, binary_fraction=1.0,
         skewing positive (below 1) or negative (over 1)
     :param float continuous_scaling_factor: scale of continuous variable value
         range relative to binary variables
-    :param float coefs_scaling_factor: determines volatility of the response
+    :param list[tuples] terms:
+    :param list[callable] interaction_funcs:
     :param float const: represents the base prediction given no other data.
         Should be entered as np.log(base_rate)
-    :param int random_state: specify for reproducible results
+    :param int random_state: specify for reproducable results
     :return tuple(pd.DataFrame, pd.Series): predictors and target variable
     """
     np.random.seed(random_state)
     data = generate_x_data(nrows, nvars, binary_fraction, binary_imbalance,
                            continuous_scaling_factor)
-    coefs = pd.Series(np.random.randn(nvars)) * coefs_scaling_factor
-    lam = np.exp(data.dot(coefs.values) + const)
+    lam = np.exp(generate_systematic_y(
+              data, terms=terms, interaction_funcs=interaction_funcs,
+              scale_to_range=scale_to_range
+          ))
     data['y_poisson'] = np.random.poisson(lam)
     X = data[[x for x in data if x.startswith('x')]]
     y = data['y_poisson']
@@ -169,7 +173,8 @@ def generate_poisson_data(nrows, nvars, binary_fraction=1.0,
 
 def generate_gamma_data(nrows, nvars, binary_fraction=1.0,
                         binary_imbalance=3, continuous_scaling_factor=0.5,
-                        coefs_scaling_factor=0.5, const=np.log(1000),
+                        terms=[(1, 1.0)], interaction_funcs=ARRAY_LIST_FUNCS,
+                        scale_to_range=(np.log(100), np.log(1000000)),
                         shape=1, random_state=None):
     """
     Generates data using a Poisson generative model
@@ -185,14 +190,16 @@ def generate_gamma_data(nrows, nvars, binary_fraction=1.0,
     :param float const: represents the base prediction given no other data.
         Should be entered as np.log(base_rate)
     :param int shape: the gamma distribution shape parameter
-    :param int random_state: specify for reproducible results
+    :param int random_state: specify for reproducable results
     :return tuple(pd.DataFrame, pd.Series): predictors and target variable
     """
     np.random.seed(random_state)
     data = generate_x_data(nrows, nvars, binary_fraction, binary_imbalance,
                            continuous_scaling_factor)
-    coefs = pd.Series(np.random.randn(nvars)) * coefs_scaling_factor
-    mu = np.exp(data.dot(coefs.values) + const)
+    mu = np.exp(generate_systematic_y(
+              data, terms=terms, interaction_funcs=interaction_funcs,
+              scale_to_range=scale_to_range
+          ))
     scale = mu / shape
     data['y_gamma'] = np.random.gamma(shape, scale)
     X = data[[x for x in data if x.startswith('x')]]
